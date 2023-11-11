3dceeeb6be89400c9ea5fe31bd9422d16fa13205
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-11-11 18:55:37 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,27 @@
-
-
-
     data = generate_random_data()
+if __name__ == "__main__":
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
     main()
+    for item in data:
+
 def generate_random_data():
+
+
+        print(f"Random Number: {item}")
 import random
+
 def main():
 if __name__ == "__main__":
-        print(f"Random Number: {item}")
+def generate_random_data():
+    for item in data:
+
+    main()
+    data = generate_random_data()
+def main():
+import random
     return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+        print(f"Random Number: {item}")
